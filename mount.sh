32fe9bec131f0959d2617314ff5da2e3a10fe05e
Kyle Harding <kmharding@pm.me>
2020-05-23 15:26:43 -0400

    exit on mount failure

--- mount.sh
+++ mount.sh
@@ -3,7 +3,7 @@
 for uuid in $(blkid -sUUID -ovalue /dev/sd??)
 do
     mkdir -v /media/"${uuid}" 2>/dev/null
-    mount -v UUID="${uuid}" /media/"${uuid}"
+    mount -v UUID="${uuid}" /media/"${uuid}" || exit 1
     chown -v "${PUID}:${PGID}" /media/"${uuid}"
 done
 
